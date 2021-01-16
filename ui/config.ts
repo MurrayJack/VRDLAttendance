f1fe64d130d33a629018d72209809908db9d495b
Murray Jack <mjack@starrez.com>
2021-01-17 09:35:52 +1100

    added some package

---
+++ ui/config.ts
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDZyJ88-G1xmDQ2DW5Ipm8flxUJtFvoFgI",
+    authDomain: "derbyattendance.firebaseapp.com",
+    projectId: "derbyattendance",
+    storageBucket: "derbyattendance.appspot.com",
+    messagingSenderId: "292421210622",
+    appId: "1:292421210622:web:bff2df1002390e138a70af"
+};
